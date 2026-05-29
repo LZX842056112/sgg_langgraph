b9181daab3723a5413c40bc61dbe7144cda2fc5e
xing <52873782+LZX842056112@users.noreply.github.com>
2026-05-29 22:52:14 +0800

    day02_GraphAPI

---
+++ 作业/Day01/day01/practice_01/state.py
@@ -0,0 +1,13 @@
+from typing import TypedDict
+
+
+class InputState(TypedDict):
+    calc_output: int
+
+
+class OutputState(TypedDict):
+    calc_output: int
+
+
+class OverAllState(TypedDict):
+    original_input: int
